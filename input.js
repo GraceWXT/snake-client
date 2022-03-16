9a91439dc90a0b10b6e42fa87c510a2248952799
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-16 23:25:18 +0000

    Added canned message when pressing g and h

--- input.js
+++ input.js
@@ -1,13 +1,46 @@
 // setup interface to handle user input from stdin
 
+// "Move: up" - move up one square (unless facing down)
+// "Move: down" - move down one square (unless facing up)
+// "Move: left" - move left one square (unless facing right)
+// "Move: right" - move left one square (unless facing left)
+
+// Stores the active TCP connection object.
+let connection;
+
+//keep handler outside for better readability, and easiler testing
 const handleUserInput = (input) => {
   if (input === '\u0003') {
     process.stdout.write('Exiting the game.\n');
     process.exit();
   }
+  if (input === "w") {
+    // console.log("You pressed 'w'.\n")
+    connection.write("Move: up");
+  }
+  if (input === "a") {
+    // console.log("You pressed 'a'.\n")
+    connection.write("Move: left");
+  }
+  if (input === "s") {
+    // console.log("You pressed 's'.\n")
+    connection.write("Move: down");
+  }
+  if (input === "d") {
+    // console.log("You pressed 'd'.\n")
+    connection.write("Move: right");
+  }
+  if (input === "h") {
+    // console.log("You pressed 'd'.\n")
+    connection.write("Say: Hello!");
+  }
+  if (input === "g") {
+    // console.log("You pressed 'd'.\n")
+    connection.write("Say: Glad to be back!");
+  }
 };
-
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding("utf8");
--- play.js
+++ play.js
@@ -1,13 +1,4 @@
 const { connect } = require("./client");
 const setupInput = require("./input");
 
-connect();
-setupInput();
-
-
-// "Move: up" - move up one square (unless facing down)
-// "Move: down" - move down one square (unless facing up)
-// "Move: left" - move left one square (unless facing right)
-// "Move: right" - move left one square (unless facing left)
-
-
+setupInput(connect());
