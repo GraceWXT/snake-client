0d4393696c577dc8b4e5e69c8d074e9d1b63c880
Grace Wang <wangxiaotian2012@gmail.com>
2022-03-16 19:32:30 +0000

    created a separate client module for the TCP connection

--- play.js
+++ play.js
@@ -1,20 +1,3 @@
-const net = require('net');
+const { connect } = require("./client");
 
-// establishes a connection with the game server
-const connect = function () {
-  const conn = net.createConnection({
-    host: '165.227.47.243', // change to IP address of computer or ngrok host if tunneling
-    port: 50541 // or change to the ngrok port if tunneling
-  });
-
-  // interpret incoming data as text
-  conn.setEncoding("utf8");
-  conn.on("data", (data) => {
-    console.log(`${data}`)
-  });
-
-  return conn;
-};
-
-console.log("Connecting ...");
 connect();
